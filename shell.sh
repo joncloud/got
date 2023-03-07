b04b405e2f73b0e62940223d0c0e2830b8746ef1
joncloud <jdberube@gmail.com>
2023-03-06 20:46:01 -0800

    Adds path to shell

--- shell.sh
+++ shell.sh
@@ -5,5 +5,6 @@ REPO_DIR=$(dirname $0 | realpath)
 dosbox-x -conf shell.conf \
   -c "mount c ." \
   -c "c:" \
+  -c "set PATH=%PATH%;C:\SRC\_G1;C:\SRC\_G2;C:\SRC\_G3;C:\SRC\UTILITY"\
   -nopromptfolder \
   -fastlaunch
