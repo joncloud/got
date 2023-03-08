53e1f81962ad3ab24c13250d3e9714a9377c81a1
joncloud <jdberube@gmail.com>
2023-03-07 21:24:07 -0800

    Writes some documentation for res man

--- README.md
+++ README.md
@@ -2,6 +2,73 @@
 
 This is a copy of the MS-DOS Game God of Thunder. The intention of this project is to build and compile the game in order to preserve compatibility with its original system requirements.
 
+## Files
+
+## _G1.EXE
+
+This program is contains all of the code to play the first episode - Part I: Serpent Surprise.
+
+## _G2.EXE
+
+This program is contains all of the code to play the first episode - Part II: Non-Stick Nognir.
+
+## _G3.EXE
+
+This program is contains all of the code to play the first episode - Part II: Lookin' for Loki.
+
+## GOTRES.DAT
+
+This contains all of the game assets necessary to play the game episodes.
+
+## LVLBUILD.EXE
+
+This program is used to manipulate screen data within `GOTRES.DAT`. By default it loads `GOTRES.DAT` out of `C:\GOT\GOTRES.DAT` and edits Episode 1's data (`SDAT1`).
+
+### Arguments
+
+* `/AREA:num` - Overrides the Episode screen data to load, e.g., num=2 will load `SDAT2`
+* `/FILE:name` - Overrides the path to the `GOTRES.DAT` file to load and save
+
+### Keyboard Shortcuts
+
+* F1 - Save
+* F2 - Load
+* F3 - BGTILE
+* F4 - Fill
+* F5 - Quit
+* CTRL+F1 - Add Actor
+* CTRL+F2 - Del Actor
+* CTRL+F3 - Flip
+* CTRL+F4 - Val
+* ALT+F1 - Inc
+* ALT+F2 - Dec
+* ALT+Left Click - Place
+* ALT+Right Click - Delete
+* SHIFT+F1 - Music
+* SHIFT+F2 - Redraw
+* SHIFT+F3 - Edit Wrap
+* SHIFT+F4 - Pack Wrap
+* Left - Shift Tile Palette Left
+* Right - Shift Tile Palette Right
+
+## RES.EXE
+
+This program provides an interface for managing the `GOTRES.DAT` archive.
+
+### Arguments
+
+RES.EXE is a command based program, which has dynamic arguments. All commands first argument is filename, e.g., `RES.EXE [filename]`. When no arguments are passed, a help dialog is displayed. The following are the various argument combinations to run the program.
+
+* `C` - Creates a new archive at `[filename]`
+* `A [src_filename] [res_name] [encode_flag]` Adds a file on disk (`[src_filename]`) to the archive with a name of `[res_name]`. When the `[encode_flag]` is non-zero, then the resource will be compressed using LZSS compression before adding to the archive.
+* `U [src_filename] [res_name] [encode_flag]` - Updates an existing entry (`[res_name]`) within the archive from a file on disk (`[src_filename]`). When the `[encode_flag]` is non-zero, then the resource will be compressed using LZSS compression before adding to the archive.
+* `E [res_name] [dest_filename]` - Extracts a resource (`[res_name]`) from the archive to disk at `[dest_filename]`.
+* `D [res_name]` - Deletes a resource (`[res_name]`) from the archive.
+* `P` - Does nothing.
+* `R [res_oldname] [res_newname]` - Renames a resource (`[res_oldname]`) in the archive with a new name (`[res_newname]`).
+* `B` - Extracts all resources with a .DAT file extension.
+* `L` - Lists all entries in the archive.
+
 ## References
 
 ### Original Source Distribution
--- src/utility/res_man.h
+++ src/utility/res_man.h
@@ -1,15 +1,17 @@
+#ifndef RES_MAN_H_
+#define RES_MAN_H_
+
 typedef struct{
-    char name[9];
-    long offset;
-    long length;   //original len
-    long original_size;
-    int  key;
+  char name[9];
+  long offset;
+  long length;   //original len
+  long original_size;
+  int  key;
 } RES_HEADER;
 
 #define RES_MAX_ENTRIES 256  //max # of elements
 
-#define RES_NO_ERROR		0
-
+#define RES_NO_ERROR         0
 #define RES_NOT_ACTIVE      -1
 #define RES_CANT_OPEN       -2
 #define RES_CANT_CLOSE      -3
@@ -32,13 +34,55 @@ extern FILE *res_fp;
 extern RES_HEADER res_header[RES_MAX_ENTRIES];
 extern int res_active;
 extern int res_changed;
-extern char far *res_lzss_buff;
+extern char far* res_lzss_buff;
 
-void res_init(char far *);
-int  res_open(char *filename);
-int  res_close(void);
-void res_encrypt(char far *buff,long len,char key);
-void res_decrypt(char far *buff,long len,char key);
+/**
+ * Initializes resource manager. This function should be
+ * called before opening a resource archive.
+ * @param buff A memory buffer used for extracting and
+ * decompressing contents. Typically this buffer should
+ * be at least 18,000 bytes long.
+ */
+void res_init(char far* buff);
+
+/**
+ * Opens a resource archive.
+ * @param filename The path to the resource archive to open.
+ * @returns Returns 1 upon success, otherwise returns an enumeration.
+ * Use the `RES_*` constants, or @ref res_error to determine a text
+ * description.
+ */
+int res_open(const char* filename);
+
+/**
+ * Closes a resource archive. If any contents were changed within the
+ * archive they will be flushed out to disk.
+ * @returns Returns 1 upon success, otherwise returns an enumeration.
+ * Use the `RES_*` constants, or @ref res_error to determine a text
+ * description.
+ */
+int res_close(void);
+
+/**
+ * Encodes the memory buffer with a simple XOR algorithm with @ref key.
+ * The function is used to encode the header entries in the resource
+ * archive. This is only ever called with a @ref key of 128.
+ * @param buff The memory buffer to encode.
+ * @param len The length of the memory buffer.
+ * @param key The key for XORing the data.
+ */
+void res_encrypt(char far* buff, long len, char key);
+
+/**
+ * Decodes the memory buffer with a simple XOR algorithm with @ref key.
+ * The function is used to decode the header entries in the resource
+ * archive. This is only ever called with a @ref key of 128.
+ * @param buff The memory buffer to decode.
+ * @param len The length of the memory buffer.
+ * @param key The key for XORing the data.
+ */
+void res_decrypt(char far* buff, long len, char key);
+
 int  res_abort(void);
 int  res_create(char *filename);
 long res_encode(char far *buff,long length);
@@ -57,4 +101,12 @@ int  res_replace_file(char *fname,char *name,int encode_flag);
 long res_length(char *name);
 long res_original_size(char *name);
 int  res_rename_file(char *oldname,char *newname);
-char *res_error(int num);
+
+/**
+ * Gets a text description for the error number.
+ * @param num The error number.
+ * @returns A string with the text description.
+ */
+char* res_error(int num);
+
+#endif
