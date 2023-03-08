4fe499c9121a24ee4ccceb0c3603eeb1099733e9
joncloud <jdberube@gmail.com>
2023-03-07 17:50:01 -0800

    power boost shell

--- shell.sh
+++ shell.sh
@@ -2,9 +2,19 @@
 
 REPO_DIR=$(dirname $0 | realpath)
 
-dosbox-x -conf shell.conf \
-  -c "mount c ." \
-  -c "c:" \
-  -c "set PATH=%PATH%;C:\SRC\_G1;C:\SRC\_G2;C:\SRC\_G3;C:\SRC\UTILITY"\
-  -nopromptfolder \
-  -fastlaunch
+if [[ "$1" != "" ]]; then
+  dosbox-x -conf shell.conf \
+    -c "mount c ." \
+    -c "c:" \
+    -c "set PATH=%PATH%;C:\SRC\_G1;C:\SRC\_G2;C:\SRC\_G3;C:\SRC\UTILITY"\
+    -c "$1"\
+    -nopromptfolder \
+    -fastlaunch
+else
+  dosbox-x -conf shell.conf \
+    -c "mount c ." \
+    -c "c:" \
+    -c "set PATH=%PATH%;C:\SRC\_G1;C:\SRC\_G2;C:\SRC\_G3;C:\SRC\UTILITY"\
+    -nopromptfolder \
+    -fastlaunch
+fi
